6a53f236b26789ca809def3795df3c7a13b94ca2
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-06-28 23:25:21 +0530

    Initial for firestore application

--- app/src/main/java/com/project/firestoreapplication/MainActivity.kt
+++ app/src/main/java/com/project/firestoreapplication/MainActivity.kt
@@ -2,10 +2,13 @@ package com.project.firestoreapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView:TextView =findViewById(R.id.textId)
+        textView.text =BuildConfig.BUILD_TYPE
     }
 }
