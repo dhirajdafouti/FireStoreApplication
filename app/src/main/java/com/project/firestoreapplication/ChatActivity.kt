5f7c958fdbd8066d2039de10e59657863bd741cc
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-06-30 01:29:19 +0530

    Initial for firestore application

--- app/src/main/java/com/project/firestoreapplication/ChatActivity.kt
+++ app/src/main/java/com/project/firestoreapplication/ChatActivity.kt
@@ -3,40 +3,84 @@ package com.project.firestoreapplication
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class ChatActivity : AppCompatActivity() {
-    lateinit var t1: ToggleButton
-    lateinit var t2: ToggleButton
-    lateinit var t3: ToggleButton
-    lateinit var t4: Button
-    lateinit var e1: EditText
-    lateinit var e2: EditText
+
+    var selectedCategory = FUNNY
+    lateinit var firebase: FirebaseFirestore
+    lateinit var addCrazyButton: ToggleButton
+    lateinit var addFunnyButton: ToggleButton
+    lateinit var addSeriousButton: ToggleButton
+    lateinit var clickPostBtn: Button
+    lateinit var addthoughttext: EditText
+    lateinit var addUserNameText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_thought)
-        t1 = findViewById(R.id.addCrazyBtn)
-        t2 = findViewById(R.id.addFunnyBtn)
-        t3 = findViewById(R.id.addSeriousBtn)
-        t4 = findViewById(R.id.addPostBtn)
-        e1 = findViewById(R.id.addThoughtTxt)
-        e2 = findViewById(R.id.addUsernameTxt)
+        addCrazyButton = findViewById(R.id.addCrazyBtn)
+        addFunnyButton = findViewById(R.id.addFunnyBtn)
+        addSeriousButton = findViewById(R.id.addSeriousBtn)
+        clickPostBtn = findViewById(R.id.addPostBtn)
+        addthoughttext = findViewById(R.id.addThoughtTxt)
+        addUserNameText = findViewById(R.id.addUsernameTxt)
+        firebase = Firebase.firestore
         handleClickListener()
     }
 
 
     private fun handleClickListener() {
-        t1.setOnClickListener {
-
+        addCrazyButton.setOnClickListener {
+            if (selectedCategory == CRAZY) {
+                addCrazyButton.isChecked = true
+                return@setOnClickListener
+            }
+            addFunnyButton.isChecked = false
+            addSeriousButton.isChecked = false
+            selectedCategory = CRAZY
         }
-        t2.setOnClickListener {
-
+        addFunnyButton.setOnClickListener {
+            if (selectedCategory == FUNNY) {
+                addSeriousButton.isChecked = true
+                return@setOnClickListener
+            }
+            addCrazyButton.isChecked = false
+            addSeriousButton.isChecked = false
+            selectedCategory = FUNNY
         }
-        t3.setOnClickListener { }
+        addSeriousButton.setOnClickListener {
+            if (selectedCategory == SERIOUS) {
+                addSeriousButton.isChecked = true
+                return@setOnClickListener
+            }
+            addFunnyButton.isChecked = false
+            addCrazyButton.isChecked = false
+            selectedCategory = SERIOUS
+        }
 
-        t4.setOnClickListener {
-            //add to the firebase project.
+        clickPostBtn.setOnClickListener {
+            val data = HashMap<String, Any>()
+            data.put("category", selectedCategory)
+            data.put("comments", 0)
+            data.put("likes", 0)
+            data.put("thoughtText", addthoughttext.text.toString())
+            data.put("username", addUserNameText.text.toString())
+            data.put("time", FieldValue.serverTimestamp())
+            firebase.collection(THOUGHTS_REF).add(data).addOnCompleteListener { success ->
+                Toast.makeText(this, "The Add to FireBase Success $success", Toast.LENGTH_LONG)
+                    .show()
+
+            }.addOnFailureListener { failure ->
+                Toast.makeText(this, "The Add to FireBase Failure $failure", Toast.LENGTH_LONG)
+                    .show()
+            }
+
         }
     }
 
