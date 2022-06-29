7aa49fcaa52130bdbef87d3bc31dffefd9282bfa
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-06-30 00:43:34 +0530

    Initial for firestore application

--- app/src/main/java/com/project/firestoreapplication/ChatActivity.kt
+++ app/src/main/java/com/project/firestoreapplication/ChatActivity.kt
@@ -1,11 +1,56 @@
 package com.project.firestoreapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ToggleButton
+import androidx.appcompat.app.AppCompatActivity
 
 class ChatActivity : AppCompatActivity() {
+    lateinit var t1: ToggleButton
+    lateinit var t2: ToggleButton
+    lateinit var t3: ToggleButton
+    lateinit var t4: Button
+    lateinit var e1: EditText
+    lateinit var e2: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        setContentView(R.layout.activity_add_thought)
+        t1 = findViewById(R.id.addCrazyBtn)
+        t2 = findViewById(R.id.addFunnyBtn)
+        t3 = findViewById(R.id.addSeriousBtn)
+        t4 = findViewById(R.id.addPostBtn)
+        e1 = findViewById(R.id.addThoughtTxt)
+        e2 = findViewById(R.id.addUsernameTxt)
+        handleClickListener()
+    }
+
+
+    private fun handleClickListener() {
+        t1.setOnClickListener {
+
+        }
+        t2.setOnClickListener {
+
+        }
+        t3.setOnClickListener { }
+
+        t4.setOnClickListener {
+            //add to the firebase project.
+        }
+    }
+
+    companion object {
+        const val THOUGHTS_REF = "thoughts"
+        const val FUNNY = "funny"
+        const val SERIOUS = "serious"
+        const val CRAZY = "crazy"
+        const val POPULAR = "popular"
+        const val CATEGORY = "category"
+        const val NUM_COMMENTS = "numComments"
+        const val NUM_LIKES = "numLikes"
+        const val THOUGHT_TXT = "thoughtTxt"
+        const val TIMESTAMP = "timestamp"
+        const val USERNAME = "username"
     }
 }
--- app/src/main/java/com/project/firestoreapplication/MainActivity.kt
+++ app/src/main/java/com/project/firestoreapplication/MainActivity.kt
@@ -2,17 +2,11 @@ package com.project.firestoreapplication
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.project.firestoreapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,22 +14,10 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
         binding.fab.setOnClickListener { view ->
             val intent = Intent(this, ChatActivity::class.java)
             startActivity(intent)
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
