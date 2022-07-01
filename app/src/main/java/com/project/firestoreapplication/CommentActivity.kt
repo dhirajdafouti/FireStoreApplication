93e17006ddbdcb950a556796fd301009a371eab5
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-07-01 13:18:09 +0530

    The Firebase authentication

---
+++ app/src/main/java/com/project/firestoreapplication/CommentActivity.kt
@@ -0,0 +1,11 @@
+package com.project.firestoreapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CommentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comments)
+    }
+}
---
+++ app/src/main/java/com/project/firestoreapplication/CreateUserActivity.kt
@@ -0,0 +1,11 @@
+package com.project.firestoreapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+}
