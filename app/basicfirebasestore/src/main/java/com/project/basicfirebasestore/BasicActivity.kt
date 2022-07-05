bdab48f91f642311354aa17a4243a02b90540e96
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-07-05 17:48:48 +0530

    The Firebase authentication

---
+++ app/basicfirebasestore/src/main/java/com/project/basicfirebasestore/BasicActivity.kt
@@ -0,0 +1,17 @@
+package com.project.basicfirebasestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BasicActivity : AppCompatActivity() {
+    //https://www.youtube.com/watch?v=HQgJvHXsNOQ&list=PLrnPJCHvNZuDrSqu-dKdDi3Q6nM-VUyxD&index=12
+    //https://youtu.be/bs_xs9-RJvc
+    //https://youtube.com/playlist?list=PLam6bY5NszYOhXkY7jOS4EQAKcQwkXrp4
+    //https://youtu.be/-Gvpf8tXpbc
+    //https://youtube.com/playlist?list=PLirRGafa75rQOi3so_ngAHqDmq_Djifwu
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic)
+    }
+}
